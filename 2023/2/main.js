b6e5b03dc00f417949b7ffc7c4737a22bd403d20
kodtjej <cadiixx@gmail.com>
2023-12-03 21:26:28 +0100

    day 2 part 2

--- 2023/2/main.js
+++ 2023/2/main.js
@@ -16,38 +16,87 @@ function part1() {
         }
 
     })
-    console.log(idsum)
+    console.log("part 1:", idsum)
 }
 
 function isGamePossible(sets) {
-    const greenregex = /\d+ green/g
-    const redregex = /\d+ red/g
-    const blueregex = /\d+ blue/g
     let result = true;
     sets.forEach(g => {
-        let greenres = g.match(greenregex)
-        if (greenres){
-            if(parseInt(greenres[0].match(regex)[0]) > 13){
+        const greenRes = getGreen(g)
+        if(greenRes){
+            if(greenRes > 13){
                 result = false;
             }
         } 
-        let redres = g.match(redregex)
-        if (redres){
-            if(parseInt(redres[0].match(regex)[0]) > 12){
+        const redRes = getRed(g)
+        if (redRes){
+            if(redRes > 12){
                 result = false;
-
             }
         } 
-        let blueres = g.match(blueregex)
-        if (blueres){
-            if(parseInt(blueres[0].match(regex)[0]) > 14){
+        let blueRes = getBlue(g)
+        if (blueRes){
+            if(blueRes > 14){
                 result = false;
-
             }
         } 
     })
-
     return result
 }
 
+function getGreen(data){
+    const greenregex = /\d+ green/g
+    let res = data.match(greenregex)
+    if(!res){
+        return 0
+    }
+    return parseInt(res[0].match(regex)[0])
+}
+
+function getRed(data){
+    const redregex = /\d+ red/g
+    let res = data.match(redregex)
+    if(!res){
+        return 0
+    }
+    return parseInt(res[0].match(regex)[0])
+}
+function getBlue(data){
+    const blueregex = /\d+ blue/g
+    let res = data.match(blueregex)
+    if(!res){
+        return 0
+    }
+    return parseInt(res[0].match(regex)[0])
+}
 part1()
+
+function part2(){
+    let plerk = inputData.map(d => d.split(':'))
+    let sum = 0;
+    plerk.forEach(game => {
+        const sets = game[1].split(";")
+        let green = 0;
+        let red = 0;
+        let blue = 0;
+        sets.forEach(g => {
+            const greeng = getGreen(g);
+            if(greeng > green){
+                green = greeng
+            }
+            const blueg = getBlue(g);
+            if(blueg > blue){
+                blue = blueg
+            }
+            const redg = getRed(g);
+            if(redg > red){
+                red = redg
+            }
+        })
+
+        sum += green * red * blue
+    })
+    console.log("part 2:",sum)
+}
+
+part2()
