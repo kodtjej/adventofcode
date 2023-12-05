8e6122221102bf65169e2ee9a423aa293cf45b0a
kodtjej <cadiixx@gmail.com>
2023-12-05 11:57:30 +0100

    day 3 part 1

--- 2023/3/main.js
+++ 2023/3/main.js
@@ -2,39 +2,47 @@ import { promises as fs } from 'fs';
 const input = await fs.readFile('input', 'utf-8');
 const inputData = input.split('\n');
 const numberRegex = /\d+/;
+let rows = inputData.map((s) => s.split(''));
 
 function part1() {
-  let rows = inputData.map((s) => s.split(''));
-  const numberSet = new Set();
-
+  // hitta siffrorna
+  // hitta delar utifrån siffrorna för att avgöra om de ska vara med
+  let numbers = [];
   rows.forEach((row, rowindex) => {
-    row.forEach((val, colindex) => {
-      if (val === '.' || numberRegex.test(val)) {
-        return;
-      }
+    for (let colindex = 0; colindex < row.length; colindex++) {
+      if (numberRegex.test(row[colindex]) && isNextToPart(rowindex, colindex)) {
+        const [num, lastIndex] = findNumber(row, colindex);
 
-      for (let i = colindex - 1; i <= colindex + 1; i++) {
-        if (numberRegex.test(rows[rowindex - 1][i])) {
-          numberSet.add(findNumber(rows[rowindex - 1], i));
-        }
-
-        if (numberRegex.test(rows[rowindex + 1][i])) {
-          numberSet.add(findNumber(rows[rowindex + 1], i));
-        }
+        numbers.push(num);
+        colindex = lastIndex;
       }
-
-      if (numberRegex.test(row[colindex - 1])) {
-        numberSet.add(findNumber(row, colindex - 1));
-      }
-      if (numberRegex.test(row[colindex + 1])) {
-        numberSet.add(findNumber(row, colindex + 1));
-      }
-    });
+    }
   });
 
-  let sum = 0;
-  numberSet.forEach((n) => (sum += n));
-  console.log(sum);
+  console.log(numbers.reduce((pv, cv) => pv + cv, 0));
+}
+
+function isNextToPart(rowindex, colindex) {
+  const isPart = new RegExp(/[^\d.\w]/); //js throwing around undefineds xD
+  for (let i = colindex - 1; i <= colindex + 1; i++) {
+    if (rows[rowindex - 1] && isPart.test(rows[rowindex - 1][i])) {
+      return true;
+    }
+
+    if (rows[rowindex + 1] && isPart.test(rows[rowindex + 1][i])) {
+      return true;
+    }
+  }
+
+  if (isPart.test(rows[rowindex][colindex - 1])) {
+    return true;
+  }
+
+  if (isPart.test(rows[rowindex][colindex + 1])) {
+    return true;
+  }
+
+  return false;
 }
 
 function findNumber(row, index) {
@@ -46,18 +54,18 @@ function findNumber(row, index) {
     }
     number = currentChar + number;
   }
+  let lastIndex = index;
 
   for (let i = index; i++; i < row.length) {
     const currentChar = row[i - 1];
     if (!numberRegex.test(currentChar)) {
       break;
     }
+    lastIndex = i;
     number = number + currentChar;
   }
-
   console.log(number);
-
-  return parseInt(number);
+  return [parseInt(number), lastIndex];
 }
 
 part1();
