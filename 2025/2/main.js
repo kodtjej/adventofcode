39c74aa26f32414a9ce89d48b0edd8e26b0f2481
Isabel <cadiixx@gmail.com>
2025-12-06 20:14:19 +0100

    day 2 part 2

--- 2025/2/main.js
+++ 2025/2/main.js
@@ -2,38 +2,71 @@ import { promises as fs } from 'fs'
 const input = await fs.readFile('input', 'utf-8')
 const inputData = input.split(',')
 
-
-
-function main() {
-	const mappedInput = inputData.map(range => range.split('-').map(id => parseInt(id)));
-	let sum = 0;
-	for (const range of mappedInput) {
-		const start = range[0];
-		const end = range[1];
-		for (let i = start; i <= end; i++) {
-			if (!isValidID(i.toString())) {
-				sum = sum + i;
-			}
-		}
-	}
-
-	console.log(sum);
-
+function part1() {
+  const mappedInput = inputData.map(range =>
+    range.split('-').map(id => parseInt(id)),
+  )
+  let sum = 0
+  for (const range of mappedInput) {
+    const start = range[0]
+    const end = range[1]
+    for (let i = start; i <= end; i++) {
+      if (!isValidID(i.toString())) {
+        sum = sum + i
+      }
+    }
+  }
+  console.log('part1: ', sum)
 }
 
 function isValidID(id) {
-	if (id.substring(0, 1) === 0) return false;
+  if (id.substring(0, 1) === 0) return false
 
-	if (id.length % 2 !== 0) {
-		return true;
-	}
-	const firstHalf = id.substring(0, id.length / 2);
-	const secondHalf = id.substring(id.length / 2);
-	console.log(`first half: ${firstHalf} second half: ${secondHalf}`);
-	if (firstHalf === secondHalf) return false;
+  if (id.length % 2 !== 0) {
+    return true
+  }
+  const firstHalf = id.substring(0, id.length / 2)
+  const secondHalf = id.substring(id.length / 2)
+  if (firstHalf === secondHalf) return false
 
-	return true;
-
+  return true
 }
 
-main();
+function part2() {
+  const mappedInput = inputData.map(range =>
+    range.split('-').map(id => parseInt(id)),
+  )
+
+  let sum = 0
+  for (const range of mappedInput) {
+    const start = range[0]
+    const end = range[1]
+    for (let i = start; i <= end; i++) {
+      const stringi = i.toString()
+      if (stringi.substring(0, 1) === '0') {
+        sum += i
+        continue
+      }
+      if (isRepeatedNumber(stringi)) {
+        sum += i
+        continue
+      }
+    }
+  }
+  console.log('part 2: ', sum)
+}
+
+function isRepeatedNumber(s) {
+  const len = s.length
+  if (len < 2) return false
+
+  for (let k = 1; k <= Math.floor(len / 2); k++) {
+    if (len % k !== 0) continue
+    const base = s.slice(0, k)
+    if (base.repeat(len / k) === s) return true
+  }
+  return false
+}
+
+part1()
+part2()
